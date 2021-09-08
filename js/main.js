5c2f3b0d4d31e19ae6dd96266c1f399a2a51a0d4
Awad2002 <awadamr33@gmail.com>
2021-09-08 16:16:05 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,100 @@
+$(document).ready(function(){
+    var owl = $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:84,
+      nav:true,
+      animateIn:true,
+      items:3,
+      autoplay:true,
+      autoplayTimeout:4000
+    });
+    
+    owl.on('changed.owl.carousel', function(event) {
+      setTimeout(function(){
+        var activeEls = $('.owl-item.active').eq(1); // .eq(1) to get the "middle image out of 3 actives"
+        setCarouselCaption( activeEls ); 
+      },1);
+    });
+  
+    function setCarouselCaption(el){
+      $(".owl-item").removeClass("target");
+      el.addClass("target");
+    }
+  
+});
+
+$(document).ready(function(){
+    $('.2020-btn').click(function(){
+      $(this).addClass('active');
+      $('.2021-btn').removeClass('active');
+      $('.2020').show();
+      $('.2021').hide();
+    })
+});
+
+$(document).ready(function(){
+    $('.2021-btn').click(function(){
+      $(this).addClass('active');
+      $('.2020-btn').removeClass('active');
+      $('.2021').show();
+      $('.2020').hide();
+    })
+});
+
+$('.zzzzzzz').owlCarousel({
+  loop:true,
+  margin:20,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:4
+      }
+  }
+})
+
+$('.btata').owlCarousel({
+  loop:true,
+  margin:20,
+  autoplay:true,
+  autoplayTimeout:1600,
+  nav:false,
+  dots:false,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:1
+      },
+      1000:{
+          items:1
+      }
+  }
+})
+
+$('.fgfg').owlCarousel({
+  loop:true,
+  margin:0,
+  autoplay:true,
+  autoplayTimeout:5000,
+  nav:false,
+  dots:false,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:3
+      }
+  }
+})
+
